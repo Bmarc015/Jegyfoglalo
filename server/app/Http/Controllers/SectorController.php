b75496d84f070f8b4e9e8f303bc2eebdb0f050db
Bmarc015 <bartamarcell0@gmail.com>
2026-01-19 09:17:11 +0100

    Migraciók létrehozása

---
+++ server/app/Http/Controllers/SectorController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sector;
+use App\Http\Requests\StoreSectorRequest;
+use App\Http\Requests\UpdateSectorRequest;
+
+class SectorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSectorRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Sector $sector)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSectorRequest $request, Sector $sector)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Sector $sector)
+    {
+        //
+    }
+}
---
+++ server/database/migrations/2026_01_16_082324_create_games_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('team_home_id')->constrained(table: 'teams')->onDelete('restrict')->nullable();
+            $table->foreignId('team_away_id')->constrained('teams')->onDelete('restrict')->nullable(); 
+            $table->dateTime('game_date')->nullable();
+            $table->unique(['team_home_id', 'team_away_id', 'game_date']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
